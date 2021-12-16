6475d6bfcad6a8db1cbf3ca2049388304889b70b
Farrel <farrel.social@gmail.com>
2021-12-17 04:50:30 +0700

    first commit

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+import pickle
+import sklearn
+import numpy as np
+
+app_gg = Flask(__name__)
+
+@app_gg.route('/', methods=['POST','GET'])
+def index():
+        if request.method == 'POST':
+                
+                with open('final_pca_pickle','rb') as c:
+                        model = pickle.load(c)
+                acousticness = float(request.form['acousticness'])
+                danceability = float(request.form['danceability'])
+                energy = float(request.form['energy'])
+                instrumentalness = float(request.form['instrumentalness'])
+                speechiness = float(request.form['speechiness'])
+                valence = float(request.form['valence'])
+                liveness = float(request.form['liveness'])
+                
+                datas = np.array((acousticness,danceability,energy,instrumentalness,speechiness,valence,liveness))
+                datas = np.reshape(datas, (1, -1))
+                pca_datas = model.transform(datas)
+                
+                with open('final_kmeans_pickle','rb') as r:
+                        model = pickle.load(r)
+                clusteringSpot = model.predict(pca_datas)    
+                
+                return render_template('hasil.html',finalData=clusteringSpot)
+        else:
+                return render_template('index.html')
+         
+if __name__ == '__main__':
+        app_gg.run()               
--- app.py
+++ app.py
@@ -3,33 +3,25 @@ import pickle
 import sklearn
 import numpy as np
 
-app_gg = Flask(__name__)
+app_kmeans = Flask(__name__)
 
-@app_gg.route('/', methods=['POST','GET'])
+@app_kmeans.route('/', methods=['POST','GET'])
 def index():
         if request.method == 'POST':
-                
-                with open('final_pca_pickle','rb') as c:
-                        model = pickle.load(c)
-                acousticness = float(request.form['acousticness'])
-                danceability = float(request.form['danceability'])
-                energy = float(request.form['energy'])
-                instrumentalness = float(request.form['instrumentalness'])
-                speechiness = float(request.form['speechiness'])
-                valence = float(request.form['valence'])
-                liveness = float(request.form['liveness'])
-                
-                datas = np.array((acousticness,danceability,energy,instrumentalness,speechiness,valence,liveness))
-                datas = np.reshape(datas, (1, -1))
-                pca_datas = model.transform(datas)
-                
-                with open('final_kmeans_pickle','rb') as r:
+                with open('kmeans_model_pickle','rb') as r:
                         model = pickle.load(r)
-                clusteringSpot = model.predict(pca_datas)    
+                        recency = int(request.form['recency'])
+                        frequency = int(request.form['frequency'])
+                        monetary = float(request.form['monetary'])
+                        records = np.array((recency,frequency,monetary))
+                        records = np.reshape(records, (1, -1))
+                        hasil = model.predict(records) 
+                        hasil = hasil[0]
+                        print('Customer Segmentation is :',hasil)   
                 
-                return render_template('hasil.html',finalData=clusteringSpot)
+                return render_template('hasil.html',result=hasil)
         else:
                 return render_template('index.html')
          
 if __name__ == '__main__':
-        app_gg.run()               
+        app_kmeans.run()               
